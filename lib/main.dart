7c7970a63ed935891271f5481b6e9ba903b3a13a
dustin <eiki300499@gmail.com>
2021-12-14 17:44:20 +0100

    testing: list superhero name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:postgres/postgres.dart';
 import 'package:super_hero/super_hero.dart';
-//import 'package:super_hero/super_hero.dart';
 
 
 void main() => runApp(OHDMEditor());
@@ -11,14 +10,58 @@ class OHDMEditor extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'OHDM Editor',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Editor for OHDM Data'),
-        ),
-        body: Center(
-          child: Text(SuperHero.random()),
-        ),
+      home: SuperHeroNameGenerator(),
+    );
+  }
+}
+
+class SuperHeroNameGenerator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SuperHeroName();
+}
+
+class _SuperHeroName extends State<SuperHeroNameGenerator> {
+  final _superHeroNames = <String>[]; // as list
+  final _hearted = <String>{}; // as dictionary
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Editor for OHDM Data'),
       ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          if (i ~/ 2 >= _superHeroNames.length) {
+            _superHeroNames.addAll(_generateSuperheroNames());
+          }
+          return _buildRow(_superHeroNames[i ~/2]);
+        }
+    );
+  }
+
+  Iterable<String> _generateSuperheroNames() {
+    List<String> myNames = [];
+
+    for (int index = 0; index <= 10; index++) {
+      myNames.add(SuperHero.random());
+    }
+
+    return myNames;
+  }
+
+  Widget _buildRow(String superHeroName) {
+    return ListTile(
+      title: Text(superHeroName),
     );
   }
 }
